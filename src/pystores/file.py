75ea9939936d6287e0c409772ecf914ce37acd72
Sebastian Pietras <me@spietras.dev>
2024-01-21 21:46:06 +0000

    Added `FileStore`

---
+++ src/pystores/file.py
@@ -0,0 +1,81 @@
+from abc import abstractmethod
+from os import SEEK_END
+from typing import IO, Generic, Protocol, TypeVar
+
+from pystores.base import Store
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+class FileNotReadableError(ValueError):
+    """Raised when the file is not readable."""
+
+    def __init__(self) -> None:
+        super().__init__("File is not readable.")
+
+
+class FileNotWritableError(ValueError):
+    """Raised when the file is not writable."""
+
+    def __init__(self) -> None:
+        super().__init__("File is not writable.")
+
+
+class FileNotSeekableError(ValueError):
+    """Raised when the file is not seekable."""
+
+    def __init__(self) -> None:
+        super().__init__("File is not seekable.")
+
+
+class Serializer(Protocol[T, R]):
+    """Serializer that serializes and deserializes the value."""
+
+    @abstractmethod
+    async def serialize(self, value: T) -> R:
+        pass
+
+    @abstractmethod
+    async def deserialize(self, value: R) -> T:
+        pass
+
+
+class FileStore(Store[T], Generic[T, R]):
+    """Store that stores the value in a file."""
+
+    def __init__(self, file: IO[R], serializer: Serializer[T, R], default: T) -> None:
+        if not file.readable():
+            raise FileNotReadableError()
+
+        if not file.writable():
+            raise FileNotWritableError()
+
+        if not file.seekable():
+            raise FileNotSeekableError()
+
+        self._file = file
+        self._serializer = serializer
+        self._default = default
+
+    def _get_size(self) -> int:
+        position = self._file.tell()
+        self._file.seek(0, SEEK_END)
+        size = self._file.tell()
+        self._file.seek(position)
+        return size
+
+    async def get(self) -> T:
+        if self._get_size() == 0:
+            await self.set(self._default)
+
+        self._file.seek(0)
+        content = self._file.read()
+        return await self._serializer.deserialize(content)
+
+    async def set(self, value: T) -> None:
+        content = await self._serializer.serialize(value)
+        self._file.seek(0)
+        self._file.truncate()
+        self._file.write(content)
+        self._file.flush()
---
+++ tests/unit/test_file.py
@@ -0,0 +1,61 @@
+from collections.abc import Generator
+from tempfile import TemporaryFile
+from typing import IO, Generic, TypeVar
+
+import pytest
+
+from pystores.file import FileStore, Serializer
+from tests.utils.unit import BaseStoreTest, StoreLifespan, StoreLifespanBuilder
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+class FileStoreLifespan(StoreLifespan[T], Generic[T, R]):
+    def __init__(self, store: FileStore[T, R]) -> None:
+        self._store = store
+
+    async def enter(self) -> FileStore[T, R]:
+        return self._store
+
+    async def exit(self) -> None:
+        return None
+
+
+class FileStoreLifespanBuilder(StoreLifespanBuilder[T], Generic[T, R]):
+    def __init__(self, file: IO[R], serializer: Serializer[T, R], default: T) -> None:
+        self._file = file
+        self._serializer = serializer
+        self._default = default
+
+    async def build(self) -> FileStoreLifespan[T, R]:
+        return FileStoreLifespan(FileStore(self._file, self._serializer, self._default))
+
+
+class IntSerializer(Serializer[int, str]):
+    async def serialize(self, value: int) -> str:
+        return str(value)
+
+    async def deserialize(self, value: str) -> int:
+        return int(value)
+
+
+class TestFileStore(BaseStoreTest[int]):
+    @pytest.fixture()
+    def serializer(self) -> IntSerializer:
+        return IntSerializer()
+
+    @pytest.fixture()
+    def builder(
+        self, serializer: IntSerializer
+    ) -> Generator[FileStoreLifespanBuilder[int, str], None, None]:
+        with TemporaryFile(mode="w+t") as file:
+            yield FileStoreLifespanBuilder(file, serializer, 0)
+
+    @pytest.fixture()
+    def value(self) -> int:
+        return 1
+
+    @pytest.fixture()
+    def other_value(self) -> int:
+        return 2
